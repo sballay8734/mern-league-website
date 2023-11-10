664dab0441d14e4022db9305445b6c2779fa8a2d
Shawn Ballay <shawnballay1@gmail.com>
2023-11-10 14:06:30 -0500

    Added reminders

--- client/src/pages/Signin/Signin.tsx
+++ client/src/pages/Signin/Signin.tsx
@@ -1,5 +1,7 @@
 import "./Signin.scss"
 
+// NEED TO USE useLazyQuery
+
 export default function Signin() {
   return <div className="page">Signin</div>
 }
--- client/src/pages/Signup/Signup.tsx
+++ client/src/pages/Signup/Signup.tsx
@@ -1,5 +1,7 @@
 import "./Signup.scss"
 
+// NEED TO USE useLazyQuery
+
 export default function Signup() {
   return <div className="page">Signup</div>
 }
