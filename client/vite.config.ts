b97d06f656e1bea14513773099bc80cac0f98b56
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-12-03 10:31:15 -0500

    ...

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,10 @@ import react from "@vitejs/plugin-react-swc"
 export default defineConfig({
   server: {
     proxy: {
-      "/api": { target: "http://localhost:3000", secure: false }
+      "/api": {
+        target: "https://mern-league-website-server.onrender.com",
+        secure: false
+      }
     }
   },
   plugins: [react()]
