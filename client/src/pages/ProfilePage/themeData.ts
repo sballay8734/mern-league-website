8bd8f7063c7ef8867ce8642c039dd48e1c1809a2
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-12-03 17:25:02 -0500

    styling and notes for tomorrow

--- client/src/pages/ProfilePage/themeData.ts
+++ client/src/pages/ProfilePage/themeData.ts
@@ -2,12 +2,9 @@ export const themeOptions = [
   { theme: "eagles", logo: "themeImgs/eagles.svg" },
   { theme: "eagles-kelly", logo: "themeImgs/eagles-kelly.jpeg" },
   { theme: "liverpool", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "union", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "chelsea", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "man-city", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "giants", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "yankees", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "usa", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "misc", logo: "themeImgs/liverpool.jpeg" },
-  { theme: "misc2", logo: "themeImgs/liverpool.jpeg" }
+  { theme: "union", logo: "themeImgs/union.png" },
+  { theme: "chelsea", logo: "themeImgs/chelsea.png" },
+  { theme: "man-city", logo: "themeImgs/man-city.png" },
+  { theme: "giants", logo: "themeImgs/giants.png" },
+  { theme: "yankees", logo: "themeImgs/yankees.png" }
 ]
