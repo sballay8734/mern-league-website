27f886158a99143e742976bde46a14d17f9ab148
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-12-03 12:14:55 -0500

    ...

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://mern-league-website-server.onrender.com",
-        secure: false
+        secure: false,
+        changeOrigin: true
       }
     }
   },
