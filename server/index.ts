2e5a484a77b32d5888781af66eaf479474d205ab
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-12-03 11:46:24 -0500

    ...

--- server/index.ts
+++ server/index.ts
@@ -26,7 +26,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: "https://mern-league-website-server.onrender.com",
+    origin: "https://mern-league-website-client.onrender.com",
     credentials: true
   })
 )
