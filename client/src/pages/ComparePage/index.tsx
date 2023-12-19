fb27daec650e05f12a32cddc5b6a9971e7ea7814
Shawn Ballay <shawnballay1@gmail.com>
2023-12-18 19:57:46 -0500

    compare page styling

--- client/src/pages/ComparePage/index.tsx
+++ client/src/pages/ComparePage/index.tsx
@@ -2,6 +2,11 @@ import { useState } from "react"
 
 import { useFetchOwnersQuery } from "../../redux/owners/ownersApi"
 import { MdCompareArrows } from "react-icons/md"
+import { FaTrophy } from "react-icons/fa6"
+import { GiLargeDress } from "react-icons/gi"
+import { FaCrown } from "react-icons/fa6"
+import { FaAngleDoubleRight } from "react-icons/fa"
+import { FaAngleDoubleLeft } from "react-icons/fa"
 
 import "./ComparePage.scss"
 
@@ -52,7 +57,106 @@ export default function ComparePage() {
         </nav>
       </div>
       <div className="compare-page-bottom">
-        <div className="placeholder">Coming Soon</div>
+        {/* <div className="placeholder">Coming Soon</div> */}
+        <div className="owner-one-selector-wrapper selector-wrapper">
+          <div className="selector-header">
+            <button className="arrow arrow-left">
+              <span>
+                <FaAngleDoubleLeft />
+              </span>
+              Prev{" "}
+            </button>
+            <div className="spacer"></div>
+            <h2 className="owner-one-name owner-name">Shawn B.</h2>
+            <div className="spacer"></div>
+            <button className="arrow arrow-right">
+              Next{" "}
+              <span>
+                <FaAngleDoubleRight />
+              </span>
+            </button>
+          </div>
+          <div className="selector-body">
+            <img src="/profileImg.png" alt="profile" />
+            <div className="main-stats-wrapper">
+              <div className="main-stats main-stats-left">
+                <h2 className="stat stat1">
+                  Championships:{" "}
+                  <span className="icons">
+                    <FaTrophy /> <FaTrophy /> <FaTrophy />
+                  </span>
+                </h2>
+                <h2 className="stat stat2">
+                  KOTH Wins:{" "}
+                  <span className="icons">
+                    <FaCrown /> <FaCrown />
+                  </span>
+                </h2>
+              </div>
+              <div className="main-stats main-stats-right">
+                <h2 className="stat stat3">
+                  Skirts:{" "}
+                  <span className="icons">
+                    <GiLargeDress />
+                  </span>
+                </h2>
+                <h2 className="stat stat4">
+                  Avg. Finish: <span className="icons">3.2</span>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="main-content-wrapper">Main Content</div>
+        <div className="owner-two-selector-wrapper selector-wrapper">
+          <div className="selector-header">
+            <button className="arrow arrow-left">
+              <span>
+                <FaAngleDoubleLeft />
+              </span>
+              Prev{" "}
+            </button>
+            <div className="spacer"></div>
+            <h2 className="owner-one-name owner-name">Don I.</h2>
+            <div className="spacer"></div>
+            <button className="arrow arrow-right">
+              Next{" "}
+              <span>
+                <FaAngleDoubleRight />
+              </span>
+            </button>
+          </div>
+          <div className="selector-body">
+            <img src="/profileImg.png" alt="profile" />
+            <div className="main-stats-wrapper">
+              <div className="main-stats main-stats-left">
+                <h2 className="stat stat1">
+                  Championships:{" "}
+                  <span className="icons">
+                    <FaTrophy />
+                  </span>
+                </h2>
+                <h2 className="stat stat2">
+                  KOTH Wins:{" "}
+                  <span className="icons">
+                    <FaCrown /> <FaCrown /> <FaCrown />
+                  </span>
+                </h2>
+              </div>
+              <div className="main-stats main-stats-right">
+                <h2 className="stat stat3">
+                  Skirts:{" "}
+                  <span className="icons">
+                    <GiLargeDress />
+                  </span>
+                </h2>
+                <h2 className="stat stat4">
+                  Avg. Finish: <span className="icons">5.1</span>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
