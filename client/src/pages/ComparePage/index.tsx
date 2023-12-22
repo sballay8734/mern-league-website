42ab03b61354440752fb9d5437b3734c0121a1e8
Shawn Ballay <shawnballay1@gmail.com>
2023-12-22 15:53:34 -0500

    styling compare page

--- client/src/pages/ComparePage/index.tsx
+++ client/src/pages/ComparePage/index.tsx
@@ -7,15 +7,19 @@ import { GiLargeDress } from "react-icons/gi"
 import { FaCrown } from "react-icons/fa6"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { FaAngleDoubleLeft } from "react-icons/fa"
+import { FaCaretUp } from "react-icons/fa"
 
 import "./ComparePage.scss"
 
 export default function ComparePage() {
   const { data, error, isLoading } = useFetchOwnersQuery()
   const [activeButton, setActiveButton] = useState<string>("h2h")
-  const [activeFilterButton, setActiveFilterButton] = useState<string>("regszn")
+  const [activeFilterButton, setActiveFilterButton] =
+    useState<string>("combined")
   // below is just to remove error
   console.log(error, isLoading, data)
+
+  // ONLY SHOW GREEN STATS (no reason to show red and green - just green will be easier to make quick assessments)
 
   return (
     <div className="page compare-page">
@@ -115,6 +119,16 @@ export default function ComparePage() {
                 <ul>
                   <li>
                     <button
+                      onClick={() => setActiveFilterButton("combined")}
+                      className={`${
+                        activeFilterButton === "combined" ? "active" : ""
+                      }`}
+                    >
+                      Combined
+                    </button>
+                  </li>
+                  <li>
+                    <button
                       onClick={() => setActiveFilterButton("regszn")}
                       className={`${
                         activeFilterButton === "regszn" ? "active" : ""
@@ -133,37 +147,75 @@ export default function ComparePage() {
                       Playoffs
                     </button>
                   </li>
-                  <li>
-                    <button
-                      onClick={() => setActiveFilterButton("combined")}
-                      className={`${
-                        activeFilterButton === "combined" ? "active" : ""
-                      }`}
-                    >
-                      Combined
-                    </button>
-                  </li>
                 </ul>
               </nav>
               <div className="h2h-content-wrapper disable-scrollbars">
                 <div className="h2h-content">
                   <div className="owner-stats owner-one-stats">
-                    <div className="cell owner-name owner-one-name">
+                    <div className="main-cell owner-name owner-one-name">
                       Shawn B.
                     </div>
                     <div className="cell record owner-one-record">2-10</div>
-                    <div className="cell avgPts">118.2</div>
-                    <div className="cell avtPtsVField">125.2</div>
-                    <div className="cell playoff-rate">72%</div>
-                    <div className="cell best-week">163</div>
-                    <div className="cell worst-week">74</div>
-                    <div className="cell total-points">1272.3</div>
+                    <div className="cell avgPts">
+                      <span className="stat-value">118.2</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-9.2</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell avtPtsVField">
+                      <span className="stat-value">125.2 </span>
+                      <div className="plus-minus-and-icon">
+                        <span className="plus-minus green">+5.1</span>
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                      </div>
+                    </div>
+                    <div className="cell playoff-rate">
+                      <span className="stat-value">72.3%</span>
+                      <div className="plus-minus-and-icon">
+                        <span className="plus-minus green">+20.3</span>
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                      </div>
+                    </div>
+                    <div className="cell best-week">
+                      <span className="stat-value">163.2</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-24.1</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell worst-week red">
+                      <span className="stat-value">74.7</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-8.5</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell total-points">
+                      <span className="stat-value">1272.3</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-157.2</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
                     <div className="cell another">145-123</div>
                     <div className="cell one-more">1272.3</div>
                     <div className="cell not-copied">145-123</div>
                   </div>
                   <div className="stat-names">
-                    <div className="cell stat stat-title">Stat</div>
+                    <div className="main-cell cell stat stat-title">Stat</div>
                     <div className="cell stat stat-one">Record</div>
                     <div className="cell stat stat-one">Avg. Pts</div>
                     <div className="cell stat stat-one">Avg. Pts v Field</div>
@@ -179,17 +231,67 @@ export default function ComparePage() {
                     <div className="cell stat stat-one">Worst Week</div>
                   </div>
                   <div className="owner-stats owner-two-stats">
-                    <div className="cell owner-name owner-two-name">Don I.</div>
+                    <div className="main-cell owner-name owner-two-name">
+                      Don I.
+                    </div>
                     <div className="cell record owner-two-record">10-2</div>
-                    <div className="cell avgPts">127.8</div>
-                    <div className="cell avtPtsVField">120.3</div>
-                    <div className="cell playoff-rate">52%</div>
-                    <div className="cell best-week">187</div>
-                    <div className="cell worst-week">82</div>
-                    <div className="cell total-points">1432.8</div>
-                    <div className="cell another">165-101</div>
+                    <div className="cell avgPts">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+9.2</span>
+                      </div>
+                      <div className="stat-value">127.8</div>
+                    </div>
+                    <div className="cell avtPtsVField">
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                        <span className="plus-minus red">-5.1</span>
+                      </div> */}
+                      <span className="stat-value">120.3</span>
+                    </div>
+                    <div className="cell playoff-rate">
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                        <span className="plus-minus red">-20.3</span>
+                      </div> */}
+                      <span className="stat-value">52.1%</span>
+                    </div>
+                    <div className="cell best-week">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+24.1</span>
+                      </div>
+                      <span className="stat-value">187.9</span>
+                    </div>
+                    <div className="cell worst-week green">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+8.5</span>
+                      </div>
+                      <span className="stat-value">82.7</span>
+                    </div>
+                    <div className="cell total-points">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+157.2</span>
+                      </div>
+                      <span className="stat-value">1432.8</span>
+                    </div>
+                    <div className="cell another">123-153</div>
                     <div className="cell one-more">1272.3</div>
-                    <div className="cell not-copied">145-123</div>
+                    <div className="cell not-copied">123-152</div>
                   </div>
                 </div>
               </div>
