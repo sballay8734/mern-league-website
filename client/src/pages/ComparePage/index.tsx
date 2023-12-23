6efbd8e9a3652dd2e0ac7ae82d4959ead9d0740a
Shawn Ballay <shawnballay1@gmail.com>
2023-12-23 13:07:13 -0500

    Compare page styling

--- client/src/pages/ComparePage/index.tsx
+++ client/src/pages/ComparePage/index.tsx
@@ -355,10 +355,303 @@ export default function ComparePage() {
                   </ul>
                 </div>
               </nav>
-              <div className="yearly-content">Yearly Content</div>
+              <div className="h2h-content-wrapper disable-scrollbars">
+                <div className="h2h-content h2h-content-wrapper">
+                  <div className="owner-stats owner-one-stats">
+                    <div className="main-cell owner-name owner-one-name">
+                      Shawn B.
+                    </div>
+                    <div className="cell record owner-one-record">2-10</div>
+                    <div className="cell avgPts">
+                      <span className="stat-value">118.2</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-9.2</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell avtPtsVField">
+                      <span className="stat-value">125.2 </span>
+                      <div className="plus-minus-and-icon">
+                        <span className="plus-minus green">+5.1</span>
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                      </div>
+                    </div>
+                    <div className="cell playoff-rate">
+                      <span className="stat-value">72.3%</span>
+                      <div className="plus-minus-and-icon">
+                        <span className="plus-minus green">+20.3</span>
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                      </div>
+                    </div>
+                    <div className="cell best-week">
+                      <span className="stat-value">163.2</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-24.1</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell worst-week red">
+                      <span className="stat-value">74.7</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-8.5</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell total-points">
+                      <span className="stat-value">1272.3</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-157.2</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell another">145-123</div>
+                    <div className="cell one-more">1272.3</div>
+                    <div className="cell not-copied">145-123</div>
+                  </div>
+                  <div className="stat-names">
+                    <div className="main-cell cell stat stat-title">Stat</div>
+                    <div className="cell stat stat-one">Record</div>
+                    <div className="cell stat stat-one">Avg. Pts</div>
+                    <div className="cell stat stat-one">Avg. Pts v Field</div>
+                    <div className="cell stat stat-one">Playoff Rate</div>
+                    <div className="cell stat stat-one">Best Week</div>
+                    <div className="cell stat stat-one">Worst Week</div>
+                    <div className="cell stat stat-one">Total Points</div>
+                    <div className="cell stat stat-one">Stat</div>
+                    <div className="cell stat stat-one">Another</div>
+                    <div className="cell stat stat-one">One More</div>
+                    <div className="cell stat stat-one">Not Copied</div>
+                    <div className="cell stat stat-one">Brand New</div>
+                    <div className="cell stat stat-one">Worst Week</div>
+                  </div>
+                  <div className="owner-stats owner-two-stats">
+                    <div className="main-cell owner-name owner-two-name">
+                      Don I.
+                    </div>
+                    <div className="cell record owner-two-record">10-2</div>
+                    <div className="cell avgPts">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+9.2</span>
+                      </div>
+                      <div className="stat-value">127.8</div>
+                    </div>
+                    <div className="cell avtPtsVField">
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                        <span className="plus-minus red">-5.1</span>
+                      </div> */}
+                      <span className="stat-value">120.3</span>
+                    </div>
+                    <div className="cell playoff-rate">
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                        <span className="plus-minus red">-20.3</span>
+                      </div> */}
+                      <span className="stat-value">52.1%</span>
+                    </div>
+                    <div className="cell best-week">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+24.1</span>
+                      </div>
+                      <span className="stat-value">187.9</span>
+                    </div>
+                    <div className="cell worst-week green">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+8.5</span>
+                      </div>
+                      <span className="stat-value">82.7</span>
+                    </div>
+                    <div className="cell total-points">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+157.2</span>
+                      </div>
+                      <span className="stat-value">1432.8</span>
+                    </div>
+                    <div className="cell another">123-153</div>
+                    <div className="cell one-more">1272.3</div>
+                    <div className="cell not-copied">123-152</div>
+                  </div>
+                </div>
+              </div>
             </div>
           ) : activeButton === "allTime" ? (
-            <div className="compare-all-time">All-Time</div>
+            <div className="compare-all-time">
+              <h2 className="all-time-header">All-Time Performance</h2>
+              <div className="h2h-content-wrapper disable-scrollbars">
+                <div className="h2h-content h2h-content-wrapper">
+                  <div className="owner-stats owner-one-stats">
+                    <div className="main-cell owner-name owner-one-name">
+                      Shawn B.
+                    </div>
+                    <div className="cell record owner-one-record">2-10</div>
+                    <div className="cell avgPts">
+                      <span className="stat-value">118.2</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-9.2</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell avtPtsVField">
+                      <span className="stat-value">125.2 </span>
+                      <div className="plus-minus-and-icon">
+                        <span className="plus-minus green">+5.1</span>
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                      </div>
+                    </div>
+                    <div className="cell playoff-rate">
+                      <span className="stat-value">72.3%</span>
+                      <div className="plus-minus-and-icon">
+                        <span className="plus-minus green">+20.3</span>
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                      </div>
+                    </div>
+                    <div className="cell best-week">
+                      <span className="stat-value">163.2</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-24.1</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell worst-week red">
+                      <span className="stat-value">74.7</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-8.5</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell total-points">
+                      <span className="stat-value">1272.3</span>
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="plus-minus red">-157.2</span>
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                      </div> */}
+                    </div>
+                    <div className="cell another">145-123</div>
+                    <div className="cell one-more">1272.3</div>
+                    <div className="cell not-copied">145-123</div>
+                  </div>
+                  <div className="stat-names">
+                    <div className="main-cell cell stat stat-title">Stat</div>
+                    <div className="cell stat stat-one">Record</div>
+                    <div className="cell stat stat-one">Avg. Pts</div>
+                    <div className="cell stat stat-one">Avg. Pts v Field</div>
+                    <div className="cell stat stat-one">Playoff Rate</div>
+                    <div className="cell stat stat-one">Best Week</div>
+                    <div className="cell stat stat-one">Worst Week</div>
+                    <div className="cell stat stat-one">Total Points</div>
+                    <div className="cell stat stat-one">Stat</div>
+                    <div className="cell stat stat-one">Another</div>
+                    <div className="cell stat stat-one">One More</div>
+                    <div className="cell stat stat-one">Not Copied</div>
+                    <div className="cell stat stat-one">Brand New</div>
+                    <div className="cell stat stat-one">Worst Week</div>
+                  </div>
+                  <div className="owner-stats owner-two-stats">
+                    <div className="main-cell owner-name owner-two-name">
+                      Don I.
+                    </div>
+                    <div className="cell record owner-two-record">10-2</div>
+                    <div className="cell avgPts">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+9.2</span>
+                      </div>
+                      <div className="stat-value">127.8</div>
+                    </div>
+                    <div className="cell avtPtsVField">
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                        <span className="plus-minus red">-5.1</span>
+                      </div> */}
+                      <span className="stat-value">120.3</span>
+                    </div>
+                    <div className="cell playoff-rate">
+                      {/* <div className="plus-minus-and-icon">
+                        <span className="arrow-icon red">
+                          <FaCaretDown />
+                        </span>
+                        <span className="plus-minus red">-20.3</span>
+                      </div> */}
+                      <span className="stat-value">52.1%</span>
+                    </div>
+                    <div className="cell best-week">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+24.1</span>
+                      </div>
+                      <span className="stat-value">187.9</span>
+                    </div>
+                    <div className="cell worst-week green">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+8.5</span>
+                      </div>
+                      <span className="stat-value">82.7</span>
+                    </div>
+                    <div className="cell total-points">
+                      <div className="plus-minus-and-icon">
+                        <span className="arrow-icon green">
+                          <FaCaretUp />
+                        </span>
+                        <span className="plus-minus green">+157.2</span>
+                      </div>
+                      <span className="stat-value">1432.8</span>
+                    </div>
+                    <div className="cell another">123-153</div>
+                    <div className="cell one-more">1272.3</div>
+                    <div className="cell not-copied">123-152</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           ) : (
             "Error"
           )}
